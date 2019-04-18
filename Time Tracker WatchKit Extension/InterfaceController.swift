f13d0a80df165cf856ce2c73c2fc5a36685e1b76
Kevin OMara <kgomara@yahoo.com>
2019-04-18 10:32:27 -0700

    Initial Commit

---
+++ Time Tracker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Time Tracker WatchKit Extension
+//
+//  Created by Kevin O'Mara on 4/18/19.
+//  Copyright © 2019 O'Mara Consulting Associates. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Time Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Tracker
+//
+//  Created by Kevin O'Mara on 4/18/19.
+//  Copyright © 2019 O'Mara Consulting Associates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
